59f3f8f83c073ad022faa9c8e943eefa992b953e
Las-No4es <61088675+Las-No4es@users.noreply.github.com>
2021-05-15 21:30:23 +0400

    first commit

---
+++ Response.js
@@ -0,0 +1,386 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - react",
+        totalResults: "39500000",
+        searchTerms: "react",
+        count: 10,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "ba18eba2b7fe9b04c",
+      },
+    ],
+    nextPage: [
+      {
+        title: "Google Custom Search - react",
+        totalResults: "39500000",
+        searchTerms: "react",
+        count: 10,
+        startIndex: 11,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "ba18eba2b7fe9b04c",
+      },
+    ],
+  },
+  context: {
+    title: "Google",
+  },
+  searchInformation: {
+    searchTime: 0.440023,
+    formattedSearchTime: "0.44",
+    totalResults: "39500000",
+    formattedTotalResults: "39,500,000",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "Fans React to the Bucs New Uniform",
+      htmlTitle: "Fans <b>React</b> to the Bucs New Uniform",
+      link: "https://www.google.com/appserve/mkt/p/AM7kBiV1bfi_McVn8Al1zomrJbvmzxlXjxvPR0BL_XOPWHNnLbKFSfsb_-ZrQLVQa4L2CQT9rgx1oUyJ9eg5pxA7s8qmMsSJj4cRLEdXI4iB3ZipuRNAr-7UzkTVUcwEc4gyi_BIXxy3RS2uVQP3lhI7CnF1q4DWh5AdKQmB39V90Iz37HOJpqAy6uVll7XxQw",
+      displayLink: "www.google.com",
+      snippet:
+        "Apr 8, 2020 ... Fans React to the Bucs New Uniform. The Bucs unveiled their new uniforms on \nTuesday after months of anticipation. See what fans had to say ...",
+      htmlSnippet:
+        "Apr 8, 2020 <b>...</b> Fans <b>React</b> to the Bucs New Uniform. The Bucs unveiled their new uniforms on <br>\nTuesday after months of anticipation. See what fans had to say&nbsp;...",
+      cacheId: "Ru3RNQx9V2cJ",
+      formattedUrl:
+        "https://www.google.com/.../AM7kBiV1bfi_ McVn8Al1zomrJbvmzxlXjxvPR0BL_XOPWHNnLbKFSfsb_- ...",
+      htmlFormattedUrl:
+        "https://www.google.com/.../AM7kBiV1bfi_ McVn8Al1zomrJbvmzxlXjxvPR0BL_XOPWHNnLbKFSfsb_- ...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSD5WILsXXF3hfcJfooTgNhLusFyB2aojgQy70UYT8NlQ_R_p5bTv7-dxmi",
+            width: "300",
+            height: "168",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/buccaneers/ojds1lmzgmd74aiczs9m",
+            "apple-itunes-app":
+              "app-id=455351343, app-argument=https://www.buccaneers.com/news/fans-react-to-the-bucs-new-uniform-2020-reaction-social-media",
+            "og:type": "website",
+            "twitter:card": "summary",
+            "twitter:title": "Fans React to the Bucs New Uniform",
+            "og:title": "Fans React to the Bucs New Uniform",
+            "og:image:type": "image/jpeg",
+            "og:description":
+              "The Bucs unveiled their new uniforms on Tuesday after months of anticipation. See what fans had to say after the reveal.",
+            "fb:pages": "95501124594",
+            "twitter:image:src":
+              "https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/buccaneers/ojds1lmzgmd74aiczs9m",
+            "twitter:site": "@https://www.buccaneers.com",
+            viewport: "width=device-width, initial-scale=1.0",
+            "twitter:description":
+              "The Bucs unveiled their new uniforms on Tuesday after months of anticipation. See what fans had to say after the reveal.",
+            "og:locale": "en-US",
+            "og:url":
+              "https://www.buccaneers.com/news/fans-react-to-the-bucs-new-uniform-2020-reaction-social-media",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/buccaneers/ojds1lmzgmd74aiczs9m",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "C08G - Macromolecular compounds obtained otherwise than by ...",
+      htmlTitle:
+        "C08G - Macromolecular compounds obtained otherwise than by ...",
+      link: "http://www.google.com/patents/sitemap/en/Sitemap/C08/C08G.html",
+      displayLink: "www.google.com",
+      snippet:
+        'More topics under "C08G - Macromolecular compounds obtained otherwise than \nby reactions only involving carbon-to-carbon unsaturated bonds" (324,449).',
+      htmlSnippet:
+        "More topics under &quot;C08G - Macromolecular compounds obtained otherwise than <br>\nby <b>reactions</b> only involving carbon-to-carbon unsaturated bonds&quot; (324,449).",
+      formattedUrl: "www.google.com/patents/sitemap/en/Sitemap/C08/C08G.html",
+      htmlFormattedUrl:
+        "www.google.com/patents/sitemap/en/Sitemap/C08/C08G.html",
+    },
+    {
+      kind: "customsearch#result",
+      title: "US1743740A - Method for making anhydrous zinc chloride by ...",
+      htmlTitle:
+        "US1743740A - Method for making anhydrous zinc chloride by ...",
+      link: "https://www.google.com/patents/US1743740",
+      displayLink: "www.google.com",
+      snippet:
+        "Experiments have shown that zinc oxide, which does not directly react with \nchlorine, may be converted into zinc chloride, by reacting the zinc oxide with \nchlorine ...",
+      htmlSnippet:
+        "Experiments have shown that zinc oxide, which does not directly <b>react</b> with <br>\nchlorine, may be converted into zinc chloride, by <b>reacting</b> the zinc oxide with <br>\nchlorine&nbsp;...",
+      formattedUrl: "https://www.google.com/patents/US1743740",
+      htmlFormattedUrl: "https://www.google.com/patents/US1743740",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin-when-crossorigin",
+            citation_patent_application_number: "US:2/652,16A",
+            citation_patent_number: "US:1743740",
+            "dc.type": "patent",
+            viewport: "width=device-width, initial-scale=1",
+            "dc.title":
+              "Method for making anhydrous zinc chloride by reacting zinc oxide with chlorine",
+            citation_pdf_url:
+              "https://patentimages.storage.googleapis.com/30/28/2b/081db156358b2e/US1743740.pdf",
+            "dc.date": "1928-03-27",
+            "dc.contributor": "Girsewald Conway Von",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title:
+        "EP2044087A2 - Grignard reactions in microreactors - Google Patents",
+      htmlTitle:
+        "EP2044087A2 - Grignard <b>reactions</b> in microreactors - Google Patents",
+      link: "https://www.google.com/patents/EP2044087A2?cl=fi",
+      displayLink: "www.google.com",
+      snippet:
+        "238000003747 Grignard reactions Methods 0.000 title claims description 13; \n238000006243 chemical reactions Methods 0.000 claims abstract description \n110 ...",
+      htmlSnippet:
+        "238000003747 Grignard <b>reactions</b> Methods 0.000 title claims description 13; <br>\n238000006243 chemical <b>reactions</b> Methods 0.000 claims abstract description <br>\n110&nbsp;...",
+      formattedUrl: "https://www.google.com/patents/EP2044087A2?cl=fi",
+      htmlFormattedUrl: "https://www.google.com/patents/EP2044087A2?cl=fi",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin-when-crossorigin",
+            citation_patent_application_number: "EP:07765155A",
+            citation_patent_publication_number: "EP:2044087:A2",
+            "dc.type": "patent",
+            citation_reference: "See references of WO  2008009378A2",
+            viewport: "width=device-width, initial-scale=1",
+            "dc.title": "Grignard reactions in microreactors",
+            "dc.date": "2007-07-11",
+            "dc.contributor": "Nikolaus Bieler",
+          },
+        ],
+        scholarlyarticle: [
+          {
+            countrycode: "EP",
+            numberwithoutcodes: "2044087",
+            countryname: "European Patent Office",
+            prioritydate: "2006-07-18",
+            title: "Grignard reactions in microreactors",
+            type: "patent",
+            applicationnumber: "EP07765155A",
+            publicationdate: "2009-04-08",
+            publicationnumber: "EP2044087A2",
+            priorartdate: "2006-07-18",
+            kindcode: "A2",
+            priorartkeywords: "reaction",
+            filingdate: "2007-07-11",
+            assigneecurrent: "Lonza AG",
+            publicationdescription:
+              "Publication of application without search report",
+            inventor: "Nikolaus Bieler",
+            pagetitle:
+              "EP2044087A2 - Grignard reactions in microreactors - Google Patents",
+            assigneeoriginal: "Lonza AG",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "US3506728A - Reactions of fluorochloroethylenes - Google Patents",
+      htmlTitle:
+        "US3506728A - <b>Reactions</b> of fluorochloroethylenes - Google Patents",
+      link: "http://www.google.com/patents/US3506728",
+      displayLink: "www.google.com",
+      snippet:
+        "United States Patent 3,506,728 REACTIONS OF FLUOROCHLOROETHYLENES \nHeinz G. Viehe, Linkebeek, and Serge Y. Delavarenne,. Wemmel, Belgium ...",
+      htmlSnippet:
+        "United States Patent 3,506,728 <b>REACTIONS</b> OF FLUOROCHLOROETHYLENES <br>\nHeinz G. Viehe, Linkebeek, and Serge Y. Delavarenne,. Wemmel, Belgium&nbsp;...",
+      formattedUrl: "www.google.com/patents/US3506728",
+      htmlFormattedUrl: "www.google.com/patents/US3506728",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin-when-crossorigin",
+            citation_patent_number: "US:3506728",
+            "dc.type": "patent",
+            citation_reference: "None",
+            viewport: "width=device-width, initial-scale=1",
+            "dc.title": "Reactions of fluorochloroethylenes",
+            citation_pdf_url:
+              "https://patentimages.storage.googleapis.com/52/1e/14/49ab17bd0d1902/US3506728.pdf",
+            "dc.date": "1967-07-24",
+            "dc.contributor": "Heinz G Viehe",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title:
+        "USD373633S - Manifold device for chemical solid phase reactions ...",
+      htmlTitle:
+        "USD373633S - Manifold device for chemical solid phase <b>reactions</b> ...",
+      link: "https://www.google.com/patents/USD373633",
+      displayLink: "www.google.com",
+      snippet:
+        "1 is a perspective view of a manifold device for chemical solid phase reactions \nshowing our new design;. FIG. 2 is a another perspective view thereof; and,. FIG.",
+      htmlSnippet:
+        "1 is a perspective view of a manifold device for chemical solid phase <b>reactions</b> <br>\nshowing our new design;. FIG. 2 is a another perspective view thereof; and,. FIG.",
+      formattedUrl: "https://www.google.com/patents/USD373633",
+      htmlFormattedUrl: "https://www.google.com/patents/USD373633",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin-when-crossorigin",
+            citation_patent_application_number: "US:29/032,682",
+            citation_patent_number: "US:D373633:S",
+            "dc.type": "patent",
+            citation_reference:
+              "Labindurtries Today--Liquid Handling Systems No. pg. given.",
+            viewport: "width=device-width, initial-scale=1",
+            "dc.title": "Manifold device for chemical solid phase reactions",
+            citation_pdf_url:
+              "https://patentimages.storage.googleapis.com/fc/c6/9a/df35ac0d926425/USD373633.pdf",
+            "dc.date": "1994-12-23",
+            "dc.contributor": "Bengt La Motte",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "DE2628347A1 - useful catalysts in polvurethane forming reactions",
+      htmlTitle:
+        "DE2628347A1 - useful catalysts in polvurethane forming <b>reactions</b>",
+      link: "http://www.google.com/patents/DE2628347A1?cl=en",
+      displayLink: "www.google.com",
+      snippet:
+        "(I) are useful catalyst in prodn. of (foamed) polyurethanes by reacting \npolyisocyanates with cpds. contg. >=2 NCO-reactive H atoms and the usual \nadditives.",
+      htmlSnippet:
+        "(I) are useful catalyst in prodn. of (foamed) polyurethanes by <b>reacting</b> <br>\npolyisocyanates with cpds. contg. &gt;=2 NCO-reactive H atoms and the usual <br>\nadditives.",
+      formattedUrl: "www.google.com/patents/DE2628347A1?cl=en",
+      htmlFormattedUrl: "www.google.com/patents/DE2628347A1?cl=en",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin-when-crossorigin",
+            citation_patent_application_number: "DE:19762628347",
+            "dc.description":
+              "of formula (I) are new. (in which R' and R\" = H or 1-4C alkyl R and R1 - 1-6C alkyl which are opt. branched, opt. interrupted by O or opt. substd. by di(m)ethylamino gps.; R2 = H, 1-6C alkyl or 6-10C aryl; and R3 = H or methyl). (I) are made e.g. by reacting N,N'-dialkyl-alkylenediamines R.NH.C(R')2-NH-R1 (II) with alpha,beta-unsatd. monocarboxylic acid (derivs.) esp. the esters, at 20-200 degrees C. (I) pref. have R' and R\" = H or CH3; R2 = H, CH3 or phenyl; and R2 = H or (if R2 = H) CH3. (I) are useful catalyst in prodn. of (foamed) polyurethanes by reacting polyisocyanates with cpds. contg. >=2 NCO-reactive H atoms and the usual additives.",
+            citation_patent_publication_number: "DE:2628347:A1",
+            "dc.type": "patent",
+            viewport: "width=device-width, initial-scale=1",
+            "dc.title":
+              "Dialkyl hexahydro-(1,4)-diazepinones - useful catalysts in polvurethane forming reactions",
+            "dc.date": "1976-06-24",
+            "dc.contributor": "Wolfgang Dr Lehmann",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title:
+        "US2612515A - Use of phosphites in esterification reactions of ...",
+      htmlTitle:
+        "US2612515A - Use of phosphites in esterification <b>reactions</b> of ...",
+      link: "http://www.google.com/patents/US2612515",
+      displayLink: "www.google.com",
+      snippet:
+        "30, 1952 UNITED STATES ATENT oFrIcs USE OF PHOSPHITES IN \nESTERIFIOAT ION REACTIONS OF SYNTHETIC ALCOHOLS Boyd E.- Hudson, \nJr., ...",
+      htmlSnippet:
+        "30, 1952 UNITED STATES ATENT oFrIcs USE OF PHOSPHITES IN <br>\nESTERIFIOAT ION <b>REACTIONS</b> OF SYNTHETIC ALCOHOLS Boyd E.- Hudson, <br>\nJr.,&nbsp;...",
+      formattedUrl: "www.google.com/patents/US2612515",
+      htmlFormattedUrl: "www.google.com/patents/US2612515",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin-when-crossorigin",
+            citation_patent_number: "US:2612515",
+            "dc.type": "patent",
+            viewport: "width=device-width, initial-scale=1",
+            "dc.relation": "US:1948281",
+            "dc.title":
+              "Use of phosphites in esterification reactions of synthetic alcohols",
+            citation_pdf_url:
+              "https://patentimages.storage.googleapis.com/99/d7/14/16d4fc82472431/US2612515.pdf",
+            "dc.date": "1949-10-22",
+            "dc.contributor": "Jr Boyd E Hudson",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "WO2002018328A1 - Process for reacting carboxylic acids and ...",
+      htmlTitle:
+        "WO2002018328A1 - Process for <b>reacting</b> carboxylic acids and ...",
+      link: "http://www.google.com/patents/WO2002018328A1?cl=pt-PT",
+      displayLink: "www.google.com",
+      snippet:
+        "Process for reacting carboxylic acids and esters ... anhydrides or amides of \nsulfonic acids or halides thereof from sulfonic acids, by reactions not involving the\n ...",
+      htmlSnippet:
+        "Process for <b>reacting</b> carboxylic acids and esters ... anhydrides or amides of <br>\nsulfonic acids or halides thereof from sulfonic acids, by <b>reactions</b> not involving the<br>\n&nbsp;...",
+      formattedUrl: "www.google.com/patents/WO2002018328A1?cl=pt-PT",
+      htmlFormattedUrl: "www.google.com/patents/WO2002018328A1?cl=pt-PT",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin-when-crossorigin",
+            citation_patent_application_number: "PC:T/US2001/026662",
+            citation_patent_publication_number: "WO:2002018328:A1",
+            "dc.type": "patent",
+            viewport: "width=device-width, initial-scale=1",
+            "dc.relation": "FR:612315:A",
+            "dc.title": "Process for reacting carboxylic acids and esters",
+            "dc.date": "2001-08-27",
+            "dc.contributor": "Michael Eugene Burns",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "US1834693A - Means of promoting chemical reactions - Google ...",
+      htmlTitle:
+        "US1834693A - Means of promoting chemical <b>reactions</b> - Google ...",
+      link: "https://www.google.com/patents/US1834693",
+      displayLink: "www.google.com",
+      snippet:
+        "1931- H. FRISCHER MEANS OF PROMOTING CHEMICAL REACTIONS Filed \nJune 11. 1927 n 1/ en fo r Herman/1 Fri: c/; e/'. Patented Dec. 1, 1931 series ...",
+      htmlSnippet:
+        "1931- H. FRISCHER MEANS OF PROMOTING CHEMICAL <b>REACTIONS</b> Filed <br>\nJune 11. 1927 n 1/ en fo r Herman/1 Fri: c/; e/&#39;. Patented Dec. 1, 1931 series&nbsp;...",
+      formattedUrl: "https://www.google.com/patents/US1834693",
+      htmlFormattedUrl: "https://www.google.com/patents/US1834693",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin-when-crossorigin",
+            citation_patent_number: "US:1834693",
+            "dc.type": "patent",
+            viewport: "width=device-width, initial-scale=1",
+            "dc.title": "Means of promoting chemical reactions",
+            citation_pdf_url:
+              "https://patentimages.storage.googleapis.com/18/78/2a/e00b2d59ce524e/US1834693.pdf",
+            "dc.date": "1927-06-11",
+            "dc.contributor": "Frischer Hermann",
+          },
+        ],
+      },
+    },
+  ],
+};
